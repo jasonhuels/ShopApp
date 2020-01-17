07bebe0607378eb9eac08e276ac3f3d904b40443
Jason Huels <jasonhuels@yahoo.com>
2020-01-17 15:35:27 -0800

    create redux store, productReducer, and import dummy data

---
+++ screens/shop/ProductOverviewScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { FlatList } from "react-native";
+
+const ProductsOverviewScreeen = props => {
+  return (
+    <FlatList>
+      
+    </FlatList>
+  );
+};
+
+export default ProductsOverviewScreen;
