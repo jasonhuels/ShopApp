ad8f6157777a8fe9214b42eff0df60635fc0f78e
Jason Huels <jasonhuels@yahoo.com>
2020-01-19 15:06:51 -0800

    create productItem component to store product information

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,18 +1,32 @@
 import React from "react";
-import { FlatList, Text } from "react-native";
+import { FlatList, Text, StyleSheet } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreeen = props => {
   const products = useSelector(state => state.products.availableProducts);
   return (
-    <FlatList data={products} keyExtractor={item => item.id} renderItem={itemData => <Text>{itemData.item.title}</Text>}>
-      
-    </FlatList>
+    <FlatList 
+      data={products} 
+      keyExtractor={item => item.id} 
+      renderItem={itemData => 
+        <ProductItem 
+          image={itemData.item.imageUrl} 
+          title={itemData.item.title} 
+          price={itemData.item.price} 
+          onViewDetail={() => {}}
+          onAddToCart={() => { }}
+        />}
+    />
   );
 };
 
 ProductsOverviewScreeen.navigationOptions = {
   headerTitle: "All Products"
 };
+
+const styles = StyleSheet.create({
+
+});
 
 export default ProductsOverviewScreeen;
