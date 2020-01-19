920d090f8c9ce6456c27aa15f0d2bce304f8b338
Jason Huels <jasonhuels@yahoo.com>
2020-01-19 15:32:50 -0800

    change productItem styles

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -3,9 +3,11 @@ import { createStackNavigator } from "react-navigation-stack";
 import ProductsOverviewScreen from "../screens/shop/ProductsOverviewScreen";
 import Colors from "../constants/Colors";
 import { Platform } from "react-native";
+import ProductDetailScreen from "../screens/shop/ProductDetailScreen";
 
 const ProductsNavigator = createStackNavigator({
-  ProductsOverview: ProductsOverviewScreen
+  ProductsOverview: ProductsOverviewScreen,
+  ProductDetail: ProductDetailScreen
 }, {
   defaultNavigationOptions: {
     headerStyle: {
--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -14,7 +14,10 @@ const ProductsOverviewScreeen = props => {
           image={itemData.item.imageUrl} 
           title={itemData.item.title} 
           price={itemData.item.price} 
-          onViewDetail={() => {}}
+          onViewDetail={() => { props.navigation.navigate("ProductDetail", { 
+            productId: itemData.item.id,
+            productTitle: itemData.item.title 
+          })}}
           onAddToCart={() => { }}
         />}
     />
